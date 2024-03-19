412a26d56c87afaf0b21b5aca891600c60261f92
dmajaa <151950981+dmajaa@users.noreply.github.com>
2024-03-19 11:41:14 +0100

    0319

--- Frontend/my-app/src/diagram.js
+++ Frontend/my-app/src/diagram.js
@@ -1,8 +1,28 @@
 import React from 'react';
-import { Bar } from 'react-chartjs-2';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
-const DiagramComponent = ({ data, options }) => {
-  return <Bar data={data} options={options} />;
-};
+const data = [
+  { name: 'Január', uv: 5, pv: 2, amt: 10 },
+  { name: 'Február', uv: 6, pv: 5, amt: 10 },
+  { name: 'Március', uv: 2, pv: 2, amt: 10 },
+  { name: 'Április', uv: 8, pv: 6, amt: 10 },
+  { name: 'Május', uv: 4, pv: 3, amt: 10 },
+  { name: 'Június', uv: 5, pv: 4, amt: 10 },
+  
+];
 
-export default DiagramComponent;
+function Diagram() {
+  return (
+    <BarChart width={600} height={300} data={data} margin={{ top: 20, right: 30, left: 20, bottom: 5 }}>
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="name" />
+      <YAxis />
+      <Tooltip />
+      <Legend />
+      <Bar dataKey="uv" fill="#008080" name= "Összes örökbefogadott állat száma" />
+      <Bar dataKey="pv" fill="#00B2CA" name="Örökbefogadott kutyák száma" />
+    </BarChart>
+  );
+}
+
+export default Diagram;
