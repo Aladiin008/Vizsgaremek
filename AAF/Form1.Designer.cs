445e8ff2258620ba48c72316011c131e92348651
dmajaa <151950981+dmajaa@users.noreply.github.com>
2024-04-10 21:23:07 +0200

    Add files via upload

---
+++ AAF/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Mancsallatmenhely
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxUsername = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.ButtonLogin = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxUsername
+            // 
+            this.textBoxUsername.Location = new System.Drawing.Point(305, 157);
+            this.textBoxUsername.Name = "textBoxUsername";
+            this.textBoxUsername.Size = new System.Drawing.Size(161, 26);
+            this.textBoxUsername.TabIndex = 0;
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(305, 202);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(161, 26);
+            this.textBoxPassword.TabIndex = 1;
+            // 
+            // ButtonLogin
+            // 
+            this.ButtonLogin.Location = new System.Drawing.Point(305, 252);
+            this.ButtonLogin.Name = "ButtonLogin";
+            this.ButtonLogin.Size = new System.Drawing.Size(142, 59);
+            this.ButtonLogin.TabIndex = 2;
+            this.ButtonLogin.Text = "Bejelentkezés";
+            this.ButtonLogin.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(179, 160);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Felhasználónév";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(220, 208);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Jelszó";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.ButtonLogin);
+            this.panel1.Controls.Add(this.textBoxPassword);
+            this.panel1.Controls.Add(this.textBoxUsername);
+            this.panel1.Location = new System.Drawing.Point(0, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(799, 450);
+            this.panel1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxUsername;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Button ButtonLogin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ AAF/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Mancsallatmenhely
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            ButtonLogin.Click += ButtonLogin_Click;
+        }
+
+        private void ButtonLogin_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                string username = textBoxUsername.Text;
+                string password = textBoxPassword.Text;
+
+
+                MySqlConnection connection = new MySqlConnection("server=localhost;user=root;password=;database=mancsallatmenhely;");
+
+                connection.Open();
+
+
+                string query = $"SELECT * FROM Felhasznalok WHERE FelhasznaloNev = '{username}' AND Jelszo = '{password}'";
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+
+                if (reader.Read())
+                {
+
+                    bool isAdmin = reader.GetBoolean("adminjogosultsag");
+                    if (isAdmin)
+                    {
+
+                        MessageBox.Show("Sikeres bejelentkezés adminként!");
+                        panel1.Visible = false;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Sajnos nem rendelkezel admin jogosultsággal!");
+                    }
+                }
+                else
+                {
+
+                    MessageBox.Show("Hibás felhasználónév vagy jelszó!");
+                }
+                connection.Close();
+            } 
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hiba történt: " + ex.Message);
+               
+            }
+            
+
+        }
+
+    }
+}
