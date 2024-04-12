578124ad248715cf41fcec889e521b345ddc7cc5
Kiss Ádám <120579516+Aladiin008@users.noreply.github.com>
2024-04-12 17:03:16 +0200

    04.12

---
+++ MancsÁllatmenhely/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace MancsÁllatmenhely
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBoxUsername = new TextBox();
+            textBoxPassword = new TextBox();
+            ButtonLogin = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            panel1 = new Panel();
+            panel2 = new Panel();
+            dataGridView1 = new DataGridView();
+            comboBox1 = new ComboBox();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // textBoxUsername
+            // 
+            textBoxUsername.Location = new Point(407, 235);
+            textBoxUsername.Margin = new Padding(4, 5, 4, 5);
+            textBoxUsername.Name = "textBoxUsername";
+            textBoxUsername.Size = new Size(213, 35);
+            textBoxUsername.TabIndex = 0;
+            // 
+            // textBoxPassword
+            // 
+            textBoxPassword.Location = new Point(407, 302);
+            textBoxPassword.Margin = new Padding(4, 5, 4, 5);
+            textBoxPassword.Name = "textBoxPassword";
+            textBoxPassword.PasswordChar = '*';
+            textBoxPassword.Size = new Size(213, 35);
+            textBoxPassword.TabIndex = 1;
+            // 
+            // ButtonLogin
+            // 
+            ButtonLogin.Location = new Point(407, 378);
+            ButtonLogin.Margin = new Padding(4, 5, 4, 5);
+            ButtonLogin.Name = "ButtonLogin";
+            ButtonLogin.Size = new Size(190, 89);
+            ButtonLogin.TabIndex = 2;
+            ButtonLogin.Text = "Bejelentkezés";
+            ButtonLogin.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(239, 240);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(154, 30);
+            label1.TabIndex = 3;
+            label1.Text = "Felhasználónév";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(293, 312);
+            label2.Margin = new Padding(4, 0, 4, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(68, 30);
+            label2.TabIndex = 4;
+            label2.Text = "Jelszó";
+            // 
+            // panel1
+            // 
+            panel1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(ButtonLogin);
+            panel1.Controls.Add(textBoxPassword);
+            panel1.Controls.Add(textBoxUsername);
+            panel1.Location = new Point(0, 5);
+            panel1.Margin = new Padding(4, 5, 4, 5);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1066, 675);
+            panel1.TabIndex = 5;
+            // 
+            // panel2
+            // 
+            panel2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            panel2.Controls.Add(dataGridView1);
+            panel2.Controls.Add(comboBox1);
+            panel2.Location = new Point(0, 8);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(1055, 655);
+            panel2.TabIndex = 5;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            dataGridView1.BackgroundColor = SystemColors.AppWorkspace;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.GridColor = SystemColors.ControlDark;
+            dataGridView1.Location = new Point(226, 4);
+            dataGridView1.MultiSelect = false;
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 72;
+            dataGridView1.Size = new Size(817, 636);
+            dataGridView1.TabIndex = 1;
+            dataGridView1.CellValueChanged += dataGridView1_CellValueChanged;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(0, 0);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(212, 38);
+            comboBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(12F, 30F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1067, 675);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Margin = new Padding(4, 5, 4, 5);
+            Name = "Form1";
+            Text = "Mancsállatmenhely";
+            Load += Form1_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxUsername;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Button ButtonLogin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private Panel panel2;
+        private DataGridView dataGridView1;
+        private ComboBox comboBox1;
+    }
+}
---
+++ MancsÁllatmenhely/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace MancsÁllatmenhely
+{
+    public partial class Form1 : Form
+    {
+        public MySqlConnection connection;
+        public string connectionString = "SERVER=localhost;DATABASE=mancsallatmenhely;UID=root;PASSWORD=;";
+
+        public Form1()
+        {
+            InitializeComponent();
+            ButtonLogin.Click += ButtonLogin_Click;
+            connection = new MySqlConnection(connectionString);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            panel2.Visible = false;
+            LoadTableNames();
+        }
+
+        private void ButtonLogin_Click(object sender, EventArgs e)
+        {
+            string username = textBoxUsername.Text;
+            string password = textBoxPassword.Text;
+
+            try
+            {
+                connection.Open();
+                string query = $"SELECT * FROM Felhasznalok WHERE FelhasznaloNev = '{username}' AND Jelszo = '{password}'";
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    bool isAdmin = reader.GetBoolean("adminjogosultsag");
+                    if (isAdmin)
+                    {
+                        MessageBox.Show("Sikeres bejelentkezés adminként!");
+                        panel2.Visible = true;
+                        comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Sajnos nem rendelkezel admin jogosultsággal!");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Hibás felhasználónév vagy jelszó!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hiba történt a bejelentkezés során: " + ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        private void LoadTableData(string tableName)
+        {
+            try
+            {
+                connection.Open();
+                string query = $"SELECT * FROM {tableName}";
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                dataGridView1.DataSource = dt;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hiba történt az adatok betöltése során: " + ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        private void LoadTableNames()
+        {
+            try
+            {
+                connection.Open();
+                DataTable dt = connection.GetSchema("Tables");
+                foreach (DataRow row in dt.Rows)
+                {
+                    string tableName = row["TABLE_NAME"].ToString() ?? "none";
+                    comboBox1.Items.Add(tableName);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hiba történt az adatbázis kapcsolódás során: " + ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string? selectedTableName = comboBox1.SelectedItem != null ? comboBox1.SelectedItem.ToString() : "NONE";
+            LoadTableData(selectedTableName);
+        }
+
+        private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dataGridView1.CurrentRow != null)
+            {
+                DataGridViewRow selectedRow = dataGridView1.Rows[e.RowIndex];
+                string primaryKeyValue = selectedRow.Cells[0].Value.ToString();
+                string columnName = dataGridView1.Columns[e.ColumnIndex].HeaderText;
+                string cellValue = selectedRow.Cells[e.ColumnIndex].Value.ToString();
+
+                try
+                {
+                    connection.Open();
+                    string tableName = comboBox1.SelectedItem.ToString();
+                    string query = $"UPDATE {tableName} SET {columnName} = '{cellValue}' WHERE {dataGridView1.Columns[0].HeaderText} = '{primaryKeyValue}'";
+                    MySqlCommand cmd = new MySqlCommand(query, connection);
+                    int rowsAffected = cmd.ExecuteNonQuery();
+                    MessageBox.Show($"{rowsAffected}  módosítás sikeresen megtörtént.");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Hiba történt az adatbázis frissítése során: " + ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+        }
+    }
+}
