71cd8d6ff98423ca432df80a01b24d29f9d8fb67
dmajaa <151950981+dmajaa@users.noreply.github.com>
2024-04-12 23:12:51 +0200

    Add files via upload

--- Frontend/my-app/src/diagram.js
+++ Frontend/my-app/src/diagram.js
@@ -29,7 +29,7 @@ function Diagram() {
 
 
   return (
-    <BarChart width={600} height={300} data={data} margin={{ top: 20, right: 30, left: 20, bottom: 5 }}>
+    <BarChart width={1200} height={300} data={data} margin={{ top: 20, right: 30, left: 20, bottom: 5 }}>
       <CartesianGrid strokeDasharray="3 3" />
       <XAxis dataKey="name" />
       <YAxis />
