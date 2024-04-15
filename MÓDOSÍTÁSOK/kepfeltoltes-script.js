76ae441a4851728cfdb672462d3347440dbfb010
dmajaa <151950981+dmajaa@users.noreply.github.com>
2024-04-15 23:26:05 +0200

    Add files via upload

---
+++ MÓDOSÍTÁSOK/kepfeltoltes-script.js
@@ -0,0 +1,40 @@
+async function feltoltes(event) {
+    event.preventDefault();
+
+    const fileInput = document.getElementById('fileInput');
+    const file = fileInput.files[0]; 
+
+    if (file) {
+        try {
+            await fileFeltoltes(file);
+        } catch (error) {
+            console.error('Hiba történt a fájl feltöltése során:', error);
+        }
+    } else {
+        console.error('Nincs fájl kiválasztva.');
+    }
+}
+
+async function fileFeltoltes(file) {
+    const formData = new FormData();
+    formData.append('image', file);
+
+    try {
+        const response = await fetch('http://127.0.0.1:8080/feltoltes', {
+            method: 'POST',
+            body: formData
+        });
+
+        if (!response.ok) {
+            throw new Error('Hiba történt a fájl feltöltése során.');
+        }
+        const data = await response.json();
+        alert('Fájl sikeresen feltöltve', data);
+        window.location.href = 'allatokszerkesztese.html';
+
+    } catch (error) {
+        alert('Nem sikerült a feltöltés', data);
+        window.location.reload();
+
+    }
+}
